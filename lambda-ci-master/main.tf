508d22d4af8eee42ae3abad8324a95fd336efcd6
Patil <rohit.r.patil@accenture.com>
2019-12-06 15:44:36 +0530

    uncommented the code

--- lambda-ci-master/main.tf
+++ lambda-ci-master/main.tf
@@ -6,35 +6,35 @@ provider "aws" {
 // S3 bucket
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.bucket}"
-  acl    = "public"
+  acl    = "private"
 }
 
 // Jenkins slave instance profile
-//resource "aws_iam_instance_profile" "worker_profile" {
-//  name = "JenkinsWorkerProfile"
-//  role = "${aws_iam_role.worker_role.name}"
-//}
+resource "aws_iam_instance_profile" "worker_profile" {
+  name = "JenkinsWorkerProfile"
+  role = "${aws_iam_role.worker_role.name}"
+}
 
-//resource "aws_iam_role" "worker_role" {
-//  name = "JenkinsBuildRole"
-//  path = "/"
-//
-//  assume_role_policy = <<EOF
-//{
-//    "Version": "2012-10-17",
-//    "Statement": [
-//        {
-//            "Action": "sts:AssumeRole",
-//            "Principal": {
-//               "Service": "ec2.amazonaws.com"
-//            },
-//            "Effect": "Allow",
-//            "Sid": ""
-//        }
-//    ]
-//}
-//EOF
-//}
+resource "aws_iam_role" "worker_role" {
+  name = "JenkinsBuildRole"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
 
 resource "aws_iam_policy" "s3_policy" {
   name = "PushToS3Policy"
@@ -79,45 +79,46 @@ resource "aws_iam_policy" "lambda_policy" {
 EOF
 }
 
-//resource "aws_iam_role_policy_attachment" "worker_s3_attachment" {
-//  role       = "${aws_iam_role.worker_role.name}"
-//  policy_arn = "${aws_iam_policy.s3_policy.arn}"
-//}
-//
-//resource "aws_iam_role_policy_attachment" "worker_lambda_attachment" {
-//  role       = "${aws_iam_role.worker_role.name}"
-//  policy_arn = "${aws_iam_policy.lambda_policy.arn}"
-//}
+resource "aws_iam_role_policy_attachment" "worker_s3_attachment" {
+  role       = "${aws_iam_role.worker_role.name}"
+  policy_arn = "${aws_iam_policy.s3_policy.arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "worker_lambda_attachment" {
+  role       = "${aws_iam_role.worker_role.name}"
+  policy_arn = "${aws_iam_policy.lambda_policy.arn}"
+}
 
 // Lambda IAM role
-//resource "aws_iam_role" "lambda_role" {
-//  name = "FibonacciFunctionRole"
-//  path = "/"
-//
-//  assume_role_policy = <<EOF
-//{
-//    "Version": "2012-10-17",
-//    "Statement": [
-//        {
-//            "Action": "sts:AssumeRole",
-//            "Principal": {
-//               "Service": "lambda.amazonaws.com"
-//            },
-//            "Effect": "Allow",
-//            "Sid": ""
-//        }
-//    ]
-//}
-//EOF
-//}
+resource "aws_iam_role" "lambda_role" {
+  name = "FibonacciFunctionRole"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "lambda.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
 
 // Lambda function
 resource "aws_lambda_function" "function" {
   filename      = "lambda-ci-master/deployment.zip"
   function_name = "Fibonacci"
-  role          = "arn:aws:iam::241273832950:role/terraform_lambda_deploy"
+  role          = "${aws_iam_role.lambda_role.arn}"
   handler       = "main"
   runtime       = "go1.x"
 }
 
 //   role          = "arn:aws:iam::241273832950:role/accenture-mcd-lambda-role"
+//   role          = "arn:aws:iam::241273832950:role/terraform_lambda_deploy"
